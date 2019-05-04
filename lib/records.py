621a8bc77ce7cc39113ccf2b4641a825399ee54a
tekt <tekt@optexture.com>
2019-05-04 13:42:58 -0700

    Update records.py

--- lib/records.py
+++ lib/records.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Union, Dict, TypeVar, Callable, List
+from typing import Union, Dict, TypeVar, Callable, List, Iterable
 
 _JsonValue = Union[str, float, int, bool, None, Dict[str, '_JsonValue'], List['_JsonValue']]
 _ValT = TypeVar('_ValT')
@@ -25,13 +25,23 @@ class JsonOpts:
 	def __init__(
 			self,
 			condense=False,
-			prefix='',
+			prefix=None,
 			capitalize=False,
-			padtuples=False):
+			tupledefaults: List=None,
+			flat=False):
 		self.condense = condense
-		self.prefix = prefix
+		self.prefix = prefix or ''
 		self.capitalize = capitalize
-		self.padtuples = padtuples
+		self.tupledefaults = tupledefaults
+		self.flat = flat
+
+	def clone(self):
+		return JsonOpts(
+			self.condense,
+			self.prefix,
+			self.capitalize,
+			self.tupledefaults,
+			self.flat)
 
 class TypeHandler:
 	_registered = []  # type: List[TypeHandler]
@@ -94,18 +104,25 @@ class ListTypeHandler(TypeHandler):
 	def toJsonValue(self, obj: _ValT, opts: JsonOpts=None):
 		if opts.condense:
 			raise NotImplementedError()
+		if opts.flat:
+			raise NotImplementedError()
 		return [
 			self.elementHandler.toJsonValue(o, opts=opts)
 			for o in (obj or [])
 		]
 
 class TupleTypeHandler(ListTypeHandler):
-	def __init__(self, handler: TypeHandler, length: int, *defaults: _ValT):
+	def __init__(
+			self,
+			handler: TypeHandler,
+			suffixes: Iterable[str]):
 		super().__init__(handler)
-		self.length = length
-		self.defaults = defaults
+		self.suffixes = list(suffixes)
+		self.length = len(self.suffixes)
 
-	def _adjustList(self, vals: List):
+	def _adjustList(self, vals: List, opts: JsonOpts):
+		if not opts or not opts.tupledefaults:
+			return vals
 		vals = vals or []
 		n = len(vals)
 		if n == self.length:
@@ -117,18 +134,33 @@ class TupleTypeHandler(ListTypeHandler):
 			for i in range(n):
 				if i < n:
 					padded.append(vals[i])
-				elif i < len(self.defaults):
-					padded.append(self.defaults[i])
+				elif i < len(opts.tupledefaults):
+					padded.append(opts.tupledefaults[i])
 				else:
-					padded.append(self.defaults[-1])
+					padded.append(opts.tupledefaults[-1])
 			return padded
 
 	def fromJsonValue(self, val: _JsonValue, opts: JsonOpts=None):
-		return self._adjustList(super().fromJsonValue(val, opts=opts))
+		if not opts:
+			opts = JsonOpts()
+		if opts.flat and isinstance(val, dict):
+			val = [
+				val.get(opts.prefix + self.suffixes[i])
+				for i in range(self.length)
+			]
+		return self._adjustList(super().fromJsonValue(val, opts=opts), opts)
 
 	def toJsonValue(self, obj: _ValT, opts: JsonOpts=None):
+		if not opts:
+			opts = JsonOpts()
+		if opts.flat:
+			return {
+				opts.prefix + self.suffixes[i]:
+					self.elementHandler.toJsonValue(o, opts=opts)
+				for i, o in enumerate(obj)
+			}
 		vals = super().toJsonValue(obj, opts=opts)
-		return self._adjustList(vals) if opts and opts.padtuples else vals
+		return self._adjustList(vals, opts)
 
 class _Registry:
 	def __init__(self):
@@ -153,9 +185,9 @@ class _Registry:
 
 	def addAll(self, *handlers: 'TypeHandler'): return [self.add(handler) for handler in handlers]
 
-	def of(self, t: type): return self._handlersbytype[t]
+	def forValue(self, t: type): return self._handlersbytype[t]
 
-	def listOf(self, t: type): return self._listhandlersbytype[t]
+	def forListOf(self, t: type): return self._listhandlersbytype[t]
 
 Registry = _Registry()
 strHandler = Registry.add(str, withlist=True)
@@ -164,24 +196,30 @@ floatHandler = Registry.add(float, withlist=True)
 intHandler = Registry.add(
 	_CustomTypeHandler(
 		int,
-		fromJson=lambda v: v if isinstance(v, int) else int(float(v)),
-	),
+		fromJson=lambda v: v if isinstance(v, int) else int(float(v))),
 	withlist=True)
-
+xyzHandler = Registry.add(TupleTypeHandler(floatHandler, suffixes='xyz'))
+uvwHandler = Registry.add(TupleTypeHandler(floatHandler, suffixes='uvw'))
+rgbaHandler = Registry.add(TupleTypeHandler(floatHandler, suffixes='rgba'))
 
 
 class AttrSchema:
 	def __init__(
 			self,
 			name: str,
-			t: type,
+			typehandler: Union[TypeHandler, type],
 			jsonname: str = None,
 			default: _ValT = None,
+			flatprefix: str = None,
 			page: str = None):
 		self.name = name
-		self.type = t
+		if isinstance(typehandler, TypeHandler):
+			self.typehandler = typehandler
+		else:
+			self.typehandler = Registry.forValue(typehandler)
 		self.jsonname = jsonname or name.lower()
 		self.default = default
+		self.flatprefix = flatprefix
 		self.page = page
 
 
