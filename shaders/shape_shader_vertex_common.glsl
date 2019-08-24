11e0cddb82e51c5129476c37307520ee28d7acbf
tekt <tekt@optexture.com>
2019-08-24 14:27:44 -0400

    fixed uv problems
    still needs cleanup

--- shaders/shape_shader_vertex_common.glsl
+++ shaders/shape_shader_vertex_common.glsl
@@ -56,15 +56,63 @@ void applyTransform(inout vec4 pos, in TransformSettings transformSettings) {
 		rotateAxis);
 }
 
-void loadVertexAttrs(
-	inout VertexAttrs attrs,
-	in TransformSettings transformSettings) {
-	loadBasicVertexAttrs(
-		attrs,
-		shapeIndex,
-		sTexParams,
-		sColors,
-		sAttrs);
+//void loadVertexAttrs(
+//	inout VertexAttrs attrs,
+//	in TransformSettings transformSettings) {
+//	loadBasicVertexAttrs(
+//		attrs,
+//		shapeIndex,
+//		sTexParams,
+//		sColors,
+//		sAttrs);
+//
+//	{ // Avoid duplicate variable defs
+//		vec3 texcoord = TDInstanceTexCoord(uv[0]);
+//		attrs.pathTexCoord.st = texcoord.st;
+//	}
+//
+//	{ // Avoid duplicate variable defs
+//		vec3 texcoord = TDInstanceTexCoord(uv[1]);
+//		attrs.globalTexCoord = texcoord;
+//	}
+//
+//	{ // Avoid duplicate variable defs
+//		vec3 texcoord = TDInstanceTexCoord(uv[2]);
+//		attrs.faceTexCoord = texcoord;
+//	}
+//
+//
+//	// First deform the vertex and normal
+//	// TDDeform always returns values in world space
+//	vec4 worldSpacePos = TDDeform(P);
+//
+//
+//	if (!attrs.visible) {
+//		worldSpacePos = vec4(-1000.0);
+//	} else {
+//		applyTransform(worldSpacePos, transformSettings);
+//	}
+//
+//	vec3 uvUnwrapCoord = TDInstanceTexCoord(TDUVUnwrapCoord());
+//	gl_Position = TDWorldToProj(worldSpacePos, uvUnwrapCoord);
+//
+//	// This is here to ensure we only execute lighting etc. code
+//	// when we need it. If picking is active we don't need lighting, so
+//	// this entire block of code will be ommited from the compile.
+//	// The TD_PICKING_ACTIVE define will be set automatically when
+//	// picking is active.
+//#ifndef TD_PICKING_ACTIVE
+//
+//	attrs.worldSpacePos.xyz = worldSpacePos.xyz;
+////	attrs.color *= TDInstanceColor(Cd);
+//
+//#else // TD_PICKING_ACTIVE
+//
+//#endif // TD_PICKING_ACTIVE
+//}
+
+void loadVertexAttrs(inout VertexAttrs attrs, in TransformSettings transformSettings) {
+//	VertexAttrs attrs;
 
 	{ // Avoid duplicate variable defs
 		vec3 texcoord = TDInstanceTexCoord(uv[0]);
@@ -81,6 +129,9 @@ void loadVertexAttrs(
 		attrs.faceTexCoord = texcoord;
 	}
 
+	attrs.shapeIndex = shapeIndex;
+	attrs.color = texelFetch(sColors, ivec2(shapeIndex, 0), 0);
+	attrs.visible = texelFetch(sAttrs, ivec2(shapeIndex, 0), 0).r > 0.5;
 
 	// First deform the vertex and normal
 	// TDDeform always returns values in world space
@@ -95,6 +146,16 @@ void loadVertexAttrs(
 
 	vec3 uvUnwrapCoord = TDInstanceTexCoord(TDUVUnwrapCoord());
 	gl_Position = TDWorldToProj(worldSpacePos, uvUnwrapCoord);
+
+	#ifdef PATH_MODE
+	attrs.pathTex = loadTexLayerAttrs(shapeIndex, attrs, 0);
+	#endif
+	#ifdef PANEL_MODE
+	attrs.texLayer1 = loadTexLayerAttrs(shapeIndex, attrs, 0);
+	attrs.texLayer2 = loadTexLayerAttrs(shapeIndex, attrs, 1);
+	attrs.texLayer3 = loadTexLayerAttrs(shapeIndex, attrs, 2);
+	attrs.texLayer4 = loadTexLayerAttrs(shapeIndex, attrs, 3);
+	#endif
 
 	// This is here to ensure we only execute lighting etc. code
 	// when we need it. If picking is active we don't need lighting, so
