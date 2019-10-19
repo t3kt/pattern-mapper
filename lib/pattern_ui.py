7805cdba588e164eed5586b42128ca9b3fe508cf
tekt <tekt@optexture.com>
2019-10-18 23:25:12 -0700

    switched to storage and deeply dependable collections instead of DAT

--- lib/pattern_ui.py
+++ lib/pattern_ui.py
@@ -1,6 +1,6 @@
 print('pattern_ui.py loading...')
 
-from typing import Callable
+from typing import Any, Callable, Dict, List, Optional, Union
 import json
 
 if False:
@@ -19,14 +19,39 @@ except ImportError:
 
 from pattern_model import ShapeState, PatternStates
 from pattern_state import ShapeStateEditor
+try:
+	from TDStoreTools import DependDict, DependList, StorageManager
+except ImportError:
+	from _stubs.TDStoreTools import DependDict, DependList, StorageManager
+
+# try:
+# 	import _stubs.TDFunctions as TDF
+# except ImportError:
+# 	TDF = op.TDModules.mod.TDFunctions
 
 class PatternStatesManager(ExtensionBase):
 	def __init__(self, ownerComp):
 		super().__init__(ownerComp)
-		self.statetable = self.op('group_shape_states')
 		self.stateeditor = self.op('shape_state_editor')  # type: ShapeStateEditor
 		self.statesjson = self.op('states_json')
-		self.isloadingstatefromtable = False
+		self.isloadingstate = False
+		self.ipars = self.op('iparStatesManager')
+
+		# TDF.createProperty(
+		# 	self,
+		# 	'groupstates',
+		# 	value=[],
+		# 	dependable='deep')
+		# if False:
+		# 	self.groupstates = DependList()  # type: DependList[DependDict]
+
+	@property
+	def groupstates(self):
+		states = self.ownerComp.fetch('groupstates', None, search=False)
+		if states is None:
+			states = DependList()
+			self.ownerComp.store('groupstates', states)
+		return states
 
 	def LoadStates(self):
 		dat = self.op('states_json')
@@ -35,9 +60,9 @@ class PatternStatesManager(ExtensionBase):
 		obj = json.loads(jsontext) if jsontext else {}
 		states = PatternStates.FromJsonDict(obj)
 		self.ClearStates()
-		self.SaveStateToRow(states.defaultshapestate or ShapeState.DefaultState())
+		self.AddState(states.defaultshapestate or ShapeState.DefaultState())
 		for state in states.groupshapestates:
-			self.SaveStateToRow(state)
+			self.AddState(state)
 
 	def SaveStates(self, filename: str=None):
 		states = self._BuildStates()
@@ -48,8 +73,7 @@ class PatternStatesManager(ExtensionBase):
 
 	def _BuildStates(self):
 		states = PatternStates()
-		for row in range(1, self.statetable.numRows):
-			state = self._GetStateFromRow(row)
+		for state in self.groupstates:
 			if not state.group and not states.defaultshapestate:
 				states.defaultshapestate = state
 			else:
@@ -57,82 +81,117 @@ class PatternStatesManager(ExtensionBase):
 		return states
 
 	def ClearStates(self):
-		self.statetable.clear()
-		self.statetable.appendRow(['Group', 'JSON'])
+		self.groupstates.clear()
 
-	def _GetStateFromRow(self, rowindex: int):
-		statename = self.statetable[rowindex, 'Group'] or ''
-		statejson = str(self.statetable[rowindex, 'JSON'] or '')
-		stateobj = json.loads(statejson) if statejson else {}
-		state = ShapeState.FromParamsDict(stateobj)
-		state.group = statename
-		return state
+	@property
+	def GroupStateCount(self):
+		return len(self.groupstates)
 
-	def SaveStateToRow(self, state: ShapeState, row: int=None):
-		if self.statetable.numRows < 0 or self.statetable.numCols < 2 or self.statetable[0,0] != 'Group' or self.statetable[0, 1] != 'JSON':
-			self.ClearStates()
-		if state is None:
-			state = ShapeState()
-		stateobj = state.ToJsonDict() or {}
-		statejson = json.dumps(stateobj)
-		if row is None or row > (self.statetable.numRows - 1):
-			self.statetable.appendRow([state.group or '', statejson])
+	@property
+	def GroupStateNames(self):
+		return [
+			statedict.get('Group', None) or ''
+			for statedict in self.groupstates
+		]
+
+	@property
+	def GroupStateButtonLabels(self):
+		return [g or '(default)' for g in self.GroupStateNames]
+
+	def _GetStateDict(self, i: int) -> 'Optional[DependDict[str, Any]]':
+		if 0 <= i < self.GroupStateCount:
+			return self.groupstates[i]
+
+	def _GetState(self, i: int) -> Optional[ShapeState]:
+		statedict = self._GetStateDict(i)
+		if statedict is not None:
+			return ShapeState.FromParamsDict(statedict)
+
+	def _SetState(self, i: int, state: ShapeState):
+		# TODO: bounds check?
+		stateobj = state.ToParamsDict()
+		self.groupstates[i].val = stateobj
+
+	def AddState(self, nameorstate: Union[str, ShapeState]):
+		if isinstance(nameorstate, str):
+			state = ShapeState(nameorstate)
 		else:
-			self.statetable[row, 'Group'] = state.group or ''
-			self.statetable[row, 'JSON'] = statejson
+			state = nameorstate
+		stateobj = state.ToParamsDict()
+		self.groupstates.append(DependDict(**stateobj))
 
 	def PromptForNewState(self):
 		def _ok(name=None):
 			if name is not None:
-				self.SaveStateToRow(ShapeState(group=name))
+				self.AddState(name)
 		_ShowPromptDialog(
 			title='Create new state',
 			text='Enter group name(s) for new state',
 			oktext='Create',
 			ok=_ok)
 
-	def PromptEditStateName(self, row: int=None):
-		if row is None:
-			row = self._GetSelectedRow()
+	def PromptEditStateName(self, i: int=None):
+		if i is None:
+			i = self._SelectedIndex
 
 		def _ok(name=None):
 			if name is not None:
-				self.RenameStateRow(row, name)
+				self.RenameState(i, name)
 		_ShowPromptDialog(
 			title='Edit state',
 			text='Enter group name(s) for state',
 			oktext='Save',
 			ok=_ok)
 
-	def RenameStateRow(self, row: int, name: str):
-		print('RenameStateRow(row: {!r}, name: {!r})'.format(row, name))
-		self.statetable[row, 'Group'] = name or ''
-		if row == self._GetSelectedRow():
-			self.stateeditor.par.Isgroup = bool(name)
-			self.stateeditor.par.Group = name or ''
+	def RenameState(self, i: int, name: str):
+		statedict = self._GetStateDict(i)
+		statedict['Group'] = name or ''
 
-	def DeleteStateRow(self, row: int):
-		self.statetable.deleteRow(row)
+	def DeleteState(self, i: int):
+		if 0 <= i < self.GroupStateCount:
+			del self.groupstates[i]
 
-	def OnSelectStateRow(self, row: int):
-		state = self._GetStateFromRow(row)
-		self.isloadingstatefromtable = True
+	def OnSelectState(self, i: int):
+		self.isloadingstate = True
 		try:
+			state = self._GetState(i) or ShapeState()
 			self.stateeditor.SetState(state, True)
 			self.stateeditor.par.Isgroup = bool(state.group)
 			self.stateeditor.par.Group = state.group or ''
 		finally:
-			self.isloadingstatefromtable = False
+			self.isloadingstate = False
 
 	def OnStateEditorParChange(self, par):
-		if self.isloadingstatefromtable:
+		if self.isloadingstate:
 			return
-		state = self.stateeditor.GetState(filtered=True, channelsonly=False)
-		row = self._GetSelectedRow()
-		self.SaveStateToRow(state, row)
+		statedict = self._SelectedStateDict
+		if statedict is None:
+			return
+		if par.name.startswith('Include'):
+			newdict = self.stateeditor.GetStateDict(filtered=True, channelsonly=False)
+			statedict.val = newdict
+		else:
+			statedict[par.name] = par.eval()
 
-	def _GetSelectedRow(self):
-		return int(self.op('iparStatesManager').par.Selectedgroupstateindex) + 1
+	@property
+	def _SelectedIndex(self):
+		return int(self.ipars.par.Selectedgroupstateindex)
+
+	@_SelectedIndex.setter
+	def _SelectedIndex(self, i):
+		self.ipars.par.Selectedgroupstateindex = i
+
+	@property
+	def _SelectedState(self):
+		return self._GetState(self._SelectedIndex)
+
+	@_SelectedState.setter
+	def _SelectedState(self, state):
+		self._SetState(self._SelectedIndex, state)
+
+	@property
+	def _SelectedStateDict(self):
+		return self._GetStateDict(self._SelectedIndex)
 
 def _ShowPromptDialog(
 		title=None,
